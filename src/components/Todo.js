08b3059b7c110a4927731dd3b4427c0031f19f6d
TruongCongToan <thienthantinhyeu968@gmail.com>
2021-04-12 11:33:57 +0700

    Save Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,23 +6,55 @@ import useStorage from '../hooks/storage';
 import { getKey } from '../lib/util';
 
 function Todo() {
-  const [items, putItems] = React.useState([
-    /* テストコード 開始 */
-    { key: getKey(), text: '日本語の宿題', done: false },
-    { key: getKey(), text: 'reactを勉強する', done: false },
-    { key: getKey(), text: '明日の準備をする', done: false },
-    /* テストコード 終了 */
-  ]);
+  const [items, putItems, clearItems] = useStorage();
+
+  const [filterMode, setFilterMode] = useState('ALL');
+
+  const displayItems = items.filter((item) => {
+    if (filterMode === 'ALL') return true;
+    if (filterMode === 'TODO') return !item.done;
+    if (filterMode === 'DONE') return item.done;
+  });
+  const handleCheck = (itemKey) => {
+    const newItems = items.map((item) => {
+      if (item.key === itemKey) {
+        item.done = !item.done;
+      }
+      return item;
+    });
+    putItems(newItems);
+  };
+  const handleAddNewTodo = (newTodoText) => {
+    const newItems = [
+      ...items,
+      { key: getKey(), text: newTodoText, done: false },
+    ];
+    putItems(newItems);
+  };
+  const handleFilter = (keyFilterMode) => {
+    setFilterMode(keyFilterMode);
+  };
+
+  const handleDeleteTodo = () => {
+    clearItems();
+  };
 
   return (
     <div className="panel">
       <div className="panel-heading">ITSS ToDoアプリ</div>
-      {items.map((item) => (
-        <TodoItem key={item.key} item={item} />
+      <Input handleAddNewTodo={handleAddNewTodo} />
+      <Filter filterMode={filterMode} handleFilter={handleFilter} />
+      {displayItems.map((item) => (
+        <TodoItem key={item.key} item={item} handleCheck={handleCheck} />
       ))}
-      <div className="panel-block">{items.length} items</div>
+      <div className="panel-block">{displayItems.length} items</div>
+      <button
+        className="button is-light is-fullwidth"
+        onClick={handleDeleteTodo}
+      >
+        全てのToDoを削除
+      </button>
     </div>
   );
 }
-
 export default Todo;
