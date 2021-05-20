341db9115487e6090152044c10292d2ffd1d097d
TruongCongToan <thienthantinhyeu968@gmail.com>
2021-05-20 09:42:39 +0700

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyANmxnksqw1hPQ2qsI3TqSum7_-sCDmYL4",
+    authDomain: "fir-sample-99103.firebaseapp.com",
+    projectId: "fir-sample-99103",
+    storageBucket: "fir-sample-99103.appspot.com",
+    messagingSenderId: "296361025396",
+    appId: "1:296361025396:web:bc91a4f625280f38ec1621"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
